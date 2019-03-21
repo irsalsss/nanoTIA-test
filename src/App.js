8c62d12ce943af598038284d3929a96a9a7a2fb3
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-22 02:14:56 +0700

    update router app configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import DetailPage from './pages/DetailPage';
+import HomePage from './pages/HomePage';
 
 class App extends Component {
-  render() {
+  render(){
     return (
-      <div className="App">
-
-      </div>
-    );
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/:title" component={DetailPage} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
   }
 }
 
