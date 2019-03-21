dcfd67b215a0e47afbc1e1be372e35ff84f09aff
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-22 01:56:04 +0700

    add post action to fetch api for homepage and infinite scroll home page

---
+++ src/components/posts/post.action.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+// Fetch Post API
+export const fetchPosts = (endpoint) => async (dispatch, getState) => {
+   return await axios.get('https://id.techinasia.com/wp-json/techinasia/3.0/posts/?page=1')
+    .then(response => dispatch({
+      type: 'POSTS_SUCCESS',
+      endpoint,
+      response
+    }))
+    .catch(error => {
+      console.log(error)
+      throw error
+    })
+}
+
+// Fetch Post API for React Infinite Scroll
+export const fetchNextPosts = (page) => async (dispatch, getState) => {
+  return await axios.get(`https://id.techinasia.com/wp-json/techinasia/3.0/posts?page=${page}`)
+    .then(response => dispatch({
+      type: 'NEXT_POSTS_SUCCESS',
+      page: page + 1,
+      response,
+    }))
+    .catch(error => {
+      console.log(error)
+      throw error
+    })
+}
