5122afcfa5fffad0795015ffda491af4f2c616f4
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-22 01:58:51 +0700

    add reducer to update the state while fetch API in action

---
+++ src/components/posts/post.reducer.js
@@ -0,0 +1,38 @@
+const initialState = {
+  isFetching: true,
+  posts: [],
+  page: 1,
+};
+
+const posts = (state = initialState, action) => {
+  switch(action.type){
+    case 'POSTS_SUCCESS':
+      return {
+        ...state,
+        isFetching: false,
+        posts: action.response.data.posts
+      }
+    case 'POSTS_FAILURE': 
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.errorMessage,
+      }
+    case 'NEXT_POSTS_SUCCESS':
+      return {
+        ...state,
+        posts: state.posts.concat(action.response.data.posts),
+        page: action.page
+      }
+    case 'NEXT_POSTS_FAILURE': 
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.errorMessage,
+      }
+    default:
+      return state
+  }
+}
+
+export default posts;
