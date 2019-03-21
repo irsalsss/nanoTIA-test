880ded2849e2669b32f38dbc7b80ebae239e10c7
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-22 02:11:22 +0700

    create homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostScroll from '../components/posts/PostScroll';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <PostScroll />
+    </div>
+  )
+}
+
+export default HomePage
