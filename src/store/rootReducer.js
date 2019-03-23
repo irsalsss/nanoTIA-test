ed144882508339d30c985d8d575a6373b24afa8e
irsal firansyah <muhamadirsal@yahoo.com>
2019-03-23 21:12:42 +0700

    combine article reducer into root reducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import posts from '../components/posts/post.reducer';
+import articles from '../components/articles/article.reducer';
 
 const rootReducer = combineReducers({
-  posts: posts
+  posts: posts,
+  articles: articles
 });
 
 export default rootReducer
